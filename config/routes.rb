a3f92b25b240a77af7fab9f89530a8c69869343d
825814 <120297399+825814@users.noreply.github.com>
2023-02-15 00:28:58 +0000

    [finish]2

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  # get 'lists/index'
+  # get 'lists/new'
+  # get 'lists/show'
+  # get 'lists/edit'
   get '/top' => "homes#top"
+  resources :lists, only: [:create, :new]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
